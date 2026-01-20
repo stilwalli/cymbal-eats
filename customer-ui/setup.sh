f73391c567eecb4d2003774e5f1970b6bc52e4f6
Shashank <shashank.tilwalli@gmail.com>
2026-01-20 12:00:24 -0600

    test

--- customer-ui/setup.sh
+++ customer-ui/setup.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# shashank t
+# shashank
 source ../config-env.sh
 
 export BASE_DIR=$PWD
